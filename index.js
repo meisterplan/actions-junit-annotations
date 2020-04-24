7d041b24d8d4fb16b56017af00c5da72032957b7
Tobias Kuhn <tobias.kuhn@meisterplan.com>
2020-04-24 11:17:08 +0200

    Reformat index.js

--- index.js
+++ index.js
@@ -4,11 +4,6 @@ const glob = require('@actions/glob');
 const parser = require('xml2json');
 const fs = require('fs');
 
-
-
-
-
-
 (async () => {
     try {
         const path = core.getInput('path');
@@ -16,7 +11,7 @@ const fs = require('fs');
         const numFailures = core.getInput('numFailures');
         const accessToken = core.getInput('access-token');
         const testSrcPath = core.getInput('testSrcPath');
-        const globber = await glob.create(path, {followSymbolicLinks: false});
+        const globber = await glob.create(path, { followSymbolicLinks: false });
 
         let numTests = 0;
         let numSkipped = 0;
@@ -24,31 +19,30 @@ const fs = require('fs');
         let numErrored = 0;
         let testDuration = 0;
 
-        let annotations = [];
-       
+        const annotations = [];
 
         for await (const file of globber.globGenerator()) {
             const data = await fs.promises.readFile(file);
-            var json = JSON.parse(parser.toJson(data));
-            if(json.testsuite) {
+            const json = JSON.parse(parser.toJson(data));
+            if (json.testsuite) {
                 const testsuite = json.testsuite;
-                testDuration +=  Number(testsuite.time);
-                numTests +=  Number(testsuite.tests);
-                numErrored +=  Number(testsuite.errors);
-                numFailed +=  Number(testsuite.failures);
-                numSkipped +=  Number(testsuite.skipped);
-                testFunction = async testcase => {
-                    if(testcase.failure) {
-                        if(annotations.length < numFailures) {
+                testDuration += Number(testsuite.time);
+                numTests += Number(testsuite.tests);
+                numErrored += Number(testsuite.errors);
+                numFailed += Number(testsuite.failures);
+                numSkipped += Number(testsuite.skipped);
+                testFunction = async (testcase) => {
+                    if (testcase.failure) {
+                        if (annotations.length < numFailures) {
                             const klass = testcase.classname.replace(/$.*/g, '').replace(/\./g, '/');
-                            const path = `${testSrcPath}${klass}.java`
+                            const path = `${testSrcPath}${klass}.java`;
 
-                            const file = await fs.promises.readFile(path, {encoding: 'utf-8'});
+                            const file = await fs.promises.readFile(path, { encoding: 'utf-8' });
                             //TODO: make this better won't deal with methods with arguments etc
                             let line = 0;
-                            const lines = file.split('\n')
-                                for(let i = 0; i < lines.length; i++) {
-                                if(lines[i].indexOf(testcase.name) >= 0) {
+                            const lines = file.split('\n');
+                            for (let i = 0; i < lines.length; i++) {
+                                if (lines[i].indexOf(testcase.name) >= 0) {
                                     line = i;
                                     break;
                                 }
@@ -61,33 +55,33 @@ const fs = require('fs');
                                 end_column: 0,
                                 annotation_level: 'failure',
                                 message: `Junit test ${testcase.name} failed ${testcase.failure.message}`,
-                              });
+                            });
                         }
                         //add
-                    }      
-                }
-
-                if(Array.isArray(testsuite.testcase)) {
-                    for(const testcase of testsuite.testcase) {
-                        await testFunction(testcase)
                     }
-                }else {
+                };
+
+                if (Array.isArray(testsuite.testcase)) {
+                    for (const testcase of testsuite.testcase) {
+                        await testFunction(testcase);
+                    }
+                } else {
                     //single test
-                    await testFunction(testsuite.testcase)
+                    await testFunction(testsuite.testcase);
                 }
             }
         }
 
         const octokit = new github.GitHub(accessToken);
         const req = {
-        ...github.context.repo,
-        ref: github.context.sha
-        }
+            ...github.context.repo,
+            ref: github.context.sha,
+        };
         const res = await octokit.checks.listForRef(req);
-    
-        const check_run_id = res.data.check_runs.filter(check => check.name === 'build')[0].id
-    
-        const annotation_level = numFailed + numErrored > 0 ?'failure': 'notice';
+
+        const check_run_id = res.data.check_runs.filter((check) => check.name === 'build')[0].id;
+
+        const annotation_level = numFailed + numErrored > 0 ? 'failure' : 'notice';
         const annotation = {
             path: 'test',
             start_line: 0,
@@ -96,7 +90,7 @@ const fs = require('fs');
             end_column: 0,
             annotation_level,
             message: `Junit Results ran ${numTests} in ${testDuration} seconds ${numErrored} Errored, ${numFailed} Failed, ${numSkipped} Skipped`,
-          };
+        };
         // const annotation = {
         //   path: 'test',
         //   start_line: 1,
@@ -107,18 +101,17 @@ const fs = require('fs');
         //   message: `[500] failure`,
         // };
 
-
         const update_req = {
             ...github.context.repo,
             check_run_id,
             output: {
-                title: "Junit Results",
+                title: 'Junit Results',
                 summary: `Num passed etc`,
-                annotations: [annotation, ...annotations]
-            }
-        }
+                annotations: [annotation, ...annotations],
+            },
+        };
         await octokit.checks.update(update_req);
     } catch (error) {
-   		core.setFailed(error.message);
+        core.setFailed(error.message);
     }
 })();
